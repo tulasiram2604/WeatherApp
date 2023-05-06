d595251f02757a473343081842b6984b660ccf86
tulasiram2604 <131224646+tulasiram2604@users.noreply.github.com>
2023-05-06 08:25:06 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const apikey = "aY75OrA8yEsAlVv2Ozffqpf8yoT2SAAY";
+const citysearch = "https://dataservice.accuweather.com/locations/v1/";
+const current = "https://dataservice.accuweather.com/currentconditions/v1/";
+
+
+export { apikey, citysearch, current };
